50ad682389222e7d2c508f4a3602f3d73f58e5d7
Lucas Rakotomalala <lucas.rakotomalala@yahoo.fr>
2022-02-09 11:29:15 +0100

    feat: post example

--- client/src/App.js
+++ client/src/App.js
@@ -16,14 +16,29 @@ const theme = {
 const NavBar = () => {
   return (
     <Grommet.Nav direction="row" background="neutral-3" pad="xxsmall">
-      <Grommet.Anchor href='.' icon={<Icons.Home color='plain'/>} />
-      <Grommet.Anchor href='profile' icon={<Icons.User color='plain'/>} />
+      <Grommet.Anchor href='.' icon={<Icons.Home color='plain' />} />
+      <Grommet.Anchor href='profile' icon={<Icons.User color='plain' />} />
     </Grommet.Nav>
   );
 }
 const Home = () => {
   return (
-    <Grommet.Text>We are in home page !</Grommet.Text>
+    <div>
+      {
+        [...Array(10)].map((_, index) => {
+          return (
+            <Grommet.Box height="medium" width="small" overflow="hidden" key={index}>
+              <Grommet.Text>Some image from random user {index + 1}</Grommet.Text>
+              <Grommet.Carousel>
+                <Grommet.Image fit="cover" src="https://via.placeholder.com/100x200.png?text=Image+Placeholder" />
+                <Grommet.Image fit="cover" src="https://via.placeholder.com/100x200.png?text=Image+Placeholder" />
+                <Grommet.Image fit="cover" src="https://via.placeholder.com/100x200.png?text=Image+Placeholder" />
+              </Grommet.Carousel>
+            </Grommet.Box>
+          );
+        })
+      }
+    </div>
   );
 }
 
@@ -37,11 +52,13 @@ export const App = () => {
   return (
     <BrowserRouter>
       <Grommet.Grommet theme={theme}>
-        <NavBar/>
-        <Routes>
-          <Route index element={<Home/>}/>
-          <Route path="/profile" element={<Profile/>}/>
-        </Routes>
+        <NavBar />
+        <Grommet.Box align='center'>
+          <Routes>
+            <Route index element={<Home />}/>
+            <Route path="/profile" element={<Profile />}/>
+          </Routes>
+        </Grommet.Box>
       </Grommet.Grommet>
     </BrowserRouter>
   );
